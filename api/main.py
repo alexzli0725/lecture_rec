3294c650f03d0e22aba5d8cbf05feba563875f10
alexzli0725 <151705696+alexzli0725@users.noreply.github.com>
2026-01-03 21:22:40 -0800

    mongodb

---
+++ api/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+from fastapi.middleware.cors import CORSMiddleware
+from bson import ObjectId
+import os
+
+app = FastAPI()
+
+MONGODB_URI = os.getenv(
+    "MONGODB_URI",
+    "mongodb+srv://alexzli0725:alexzli0725@cluster0.y452mcg.mongodb.net/?retryWrites=true&w=majority",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client = MongoClient(MONGODB_URI)
+db = client["mydatabase"]
+collection = db["courses"]
+
+
+# Recursive function to convert ObjectId to str
+def convert_objectid(doc):
+    if isinstance(doc, list):
+        return [convert_objectid(item) for item in doc]
+    elif isinstance(doc, dict):
+        return {k: convert_objectid(v) for k, v in doc.items()}
+    elif isinstance(doc, ObjectId):
+        return str(doc)
+    else:
+        return doc
+
+
+@app.get("/courses")
+def get_all_courses():
+    courses = list(collection.find({}))
+    courses = convert_objectid(courses)
+    return courses
